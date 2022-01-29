3d18c6d7e3a4d88de0b2abe86b0c675b4a434b5e
fight2escape <fight2escape@gmail.com>
2022-01-29 11:18:27 +0800

    feat 2

---
+++ src/com/fight2escape/Question/NO2_twoSum.java
@@ -0,0 +1,28 @@
+package com.fight2escape.Question;
+
+import java.util.HashMap;
+
+class Solution {
+    public static void main(String[] args) {
+        int[] res = (new Solution()).twoSum(new int[]{2,7,11,15}, 9);
+        for (int i = 0; i < res.length; i++) {
+            System.out.println(res[i]);
+        }
+    }
+
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> m = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            m.put(nums[i], i);
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            int key = target - nums[i];
+            if (m.containsKey(key) && m.get(key) != i) {
+                return new int[]{i, m.get(key)};
+            }
+        }
+
+        return new int[]{};
+    }
+}
