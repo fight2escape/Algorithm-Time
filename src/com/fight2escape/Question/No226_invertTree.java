c46648b81a4d30e781f9e17dfcffe27f38e7ba2e
fight2escape <fight2escape@gmail.com>
2022-02-16 18:01:18 +0800

    226. 翻转二叉树

---
+++ src/com/fight2escape/Question/No226_invertTree.java
@@ -0,0 +1,21 @@
+package com.fight2escape.Question;
+
+public class No226_invertTree {
+    public static void main(String[] args) {
+
+    }
+
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return root;
+        }
+
+        TreeNode tmp = root.left;
+        root.left = root.right;
+        root.right = tmp;
+
+        invertTree(root.left);
+        invertTree(root.right);
+        return root;
+    }
+}
